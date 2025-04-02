ff5342fb9a482e47c3bec942f262b1f493c9cf1e
Jesse B. Miller <jesse@jessebmiller.com>
2025-04-01 19:04:42 -0500

    correct use of Cow strings

--- volition-agent-core/src/agent.rs
+++ volition-agent-core/src/agent.rs
@@ -312,11 +312,7 @@ impl<UI: UserInteraction + 'static> Agent<UI> {
                             let schema_map = mcp_tool.input_schema.as_ref();
                             ToolDefinition {
                                 name: mcp_tool.name.to_string(),
-                                description: mcp_tool
-                                    .description
-                                    .as_ref()
-                                    .map(|s| s.to_string())
-                                    .unwrap_or_default(), // Fixed: Use as_ref() and handle Option
+                                description: mcp_tool.description.to_string(),
                                 parameters: mcp_schema_to_tool_params(Some(schema_map)),
                             }
                         })
